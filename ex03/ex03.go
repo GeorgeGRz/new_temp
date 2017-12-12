af3d3b2c213698bee232d6c864261502d215a63b
GeorgeGRz <moustak85@hotmail.com>
2017-12-12 17:08:36 +0200

    new

--- ex03/ex03.go
+++ ex03/ex03.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
+	"sync/atomic"
 )
 
 func main() {
 	var wg sync.WaitGroup
-	counter := 0
-
+	//var mu sync.Mutex
+	//counter := 0
+	var counter2 int64
 	const gs = 50
 
 	wg.Add(gs)
@@ -17,10 +19,16 @@ func main() {
 	fmt.Println(runtime.NumCPU())
 	for i := 0; i < gs; i++ {
 		go func() {
-			v := counter
-			runtime.Gosched()
-			v++
-			counter = v
+			atomic.AddInt64(&counter2, 1)
+			/*
+				mu.Lock()
+				v := counter
+				//runtime.Gosched()
+				v++
+				counter = v
+				mu.Unlock()
+					wg.Done()*/
+			fmt.Println(atomic.LoadInt64(&counter2))
 			wg.Done()
 		}()
 		fmt.Println("Goroutines:", runtime.NumGoroutine())
@@ -29,6 +37,6 @@ func main() {
 	wg.Wait()
 	fmt.Println("===================================")
 	fmt.Println("Goroutines:", runtime.NumGoroutine())
-	fmt.Println("count:", counter)
+	fmt.Println("count:", counter2)
 
 }
